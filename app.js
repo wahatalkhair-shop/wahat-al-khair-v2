000a046d7fb030c01cabe8106f0aff016434d006
wahatalkhair-shop <mdmahmudmahim@gmail.com>
2026-06-30 20:21:23 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log("Wahat Al Khair V2 Loaded");
+
+const slides=document.querySelectorAll(".slide");
+
+let current=0;
+
+setInterval(()=>{
+
+slides[current].classList.remove("active");
+
+current++;
+
+if(current>=slides.length){
+current=0;
+}
+
+slides[current].classList.add("active");
+
+},3000);
